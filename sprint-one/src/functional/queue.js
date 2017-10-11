308534eac11930d53237b1bf60767414fcd8664f
Michael Neveu <mbneveu@gmail.com>
2017-10-11 14:34:41 -0700

    functional stack and queue working... almost" git status ;

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,14 +8,33 @@ var Queue = function() {
 
   someInstance.enqueue = function(value) {
     counter += 1;
-    sotrage[counter] = value;
+    storage[counter] = value;
+    console.log(storage);
   };
 
   someInstance.dequeue = function() {
+    var deletedItem = storage[1];
+    delete storage[1];
+    counter -= 1;
+    console.log('PRE K Rename = ', storage);
+    for (var k in storage) {
+      // k = k - 1;
+      var r = Number(k) - 1;
+      storage[r] = storage[k]; 
+      console.log('run');
+    }
+    console.log('POST K Rename =', storage);
+    return deletedItem;
+    
+    
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
 };
+
+
+
