acbeff85500f72dd36dd00e605e87e80d06ee6ab
mbntex <mbneveu@gmail.com>
2017-10-12 11:33:28 -0700

    linked list and tree completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var currentNode = Node(value);
+    if (list.head === null) {
+      list.head = currentNode;
+      list.tail = currentNode;
+    } else {
+      list.tail.next = currentNode;
+      list.tail = currentNode;
+    }
+    
   };
 
   list.removeHead = function() {
+    var valueHere = list.head.value;
+    list.head = list.head.next;
+    return valueHere;
   };
 
   list.contains = function(target) {
-  };
+    var wasFound = false;
+    var currentSearched = list.head;
+    
 
+    //as long as the next is not null, you need to...
+    while (currentSearched.next !== null && currentSearched !== list.tail) {
+      //check if value = target
+      if (currentSearched.value === target) {
+        wasFound = true;
+      }
+      //if not, change variable to next and repeat
+      currentSearched = currentSearched.next;      
+    }
+    if (list.tail.value === target) {
+      wasFound = true;
+    }
+    return wasFound;
+  };
   return list;
 };
 
