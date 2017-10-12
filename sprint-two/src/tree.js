11fa43b6f261301677f9c7511b466d2b9843bc18
mbntex <mbneveu@gmail.com>
2017-10-12 11:26:16 -0700

    completed Linked List and Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,36 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newNode = Tree(value);
+  this.children.push(newNode);
+  return newNode;
 };
 
 treeMethods.contains = function(target) {
+  /// boolean true or false
+  var result = false;
+  var parseTree = function(node) {
+    if (node.value === target) {
+      result = true;
+    }
+    if (node.children.length > 0) {
+      for (var i = 0; i < node.children.length; i++) {
+        parseTree(node.children[i]);
+      }
+    }
+  };
+  parseTree(this);
+  return result;
+  
 };
 
 
