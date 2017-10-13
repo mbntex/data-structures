a8ab3d3e978f117af42e2bd29af54f86c467bff3
mbntex <mbneveu@gmail.com>
2017-10-13 12:17:31 -0700

    BinarySearchTree setup

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,17 @@
 var BinarySearchTree = function(value) {
+  this.value = value; 
+  this.left = null;
+  this.right = null; 
+};
+
+BinarySearchTree.prototype.insert = function(val) {
+  var currentNode = new BinarySearchTree(val);
+  if (val >= this.value && this.right === null) {
+    this.right = currentNode;
+  }
+  if (val < this.value && this.left === null) {
+    this.left = currentNode;
+  }
 };
 
 
